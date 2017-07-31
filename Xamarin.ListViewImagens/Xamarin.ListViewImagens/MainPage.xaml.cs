9e3aee52b7ed7db4191488ef968e23554091f594
jeffaraujo <jeff.araujo@gmail.com>
2017-07-31 20:43:14 -0300

    Add project files.

---
+++ Xamarin.ListViewImagens/Xamarin.ListViewImagens/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xamarin.ListViewImagens
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            var lstElementos = new List<Model>();
+
+            for (var i = 0; i < 10; i++)
+            {
+                lstElementos.Add(new Model() {Texto = $"texto{i+1}" , Detalhe = $"imagem{i+1}", Imagem = "imagem"});
+            }
+
+            ListViewTexto.ItemsSource = new ObservableCollection<Model>(lstElementos);
+            ListViewImagem.ItemsSource = new ObservableCollection<Model>(lstElementos);
+        }
+    }
+}
---
+++ Xamarin.ListViewImagens/Xamarin.ListViewImagens/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamarin.ListViewImagens
+{
+    public class Model
+    {
+        public string Texto { get; set; }
+
+        public string Detalhe { get; set; }
+
+        public string Imagem { get; set; }
+
+    }
+}
